e9f873635620085da30a21584557aed5eb6d65da
irving2291 <irving2291@gmail.com>
2021-10-01 11:21:29 -0500

    fix algorithm sumRecursive

--- app/src/Payroll/Domain/Payment.php
+++ app/src/Payroll/Domain/Payment.php
@@ -19,49 +19,59 @@ final class Payment
         $this->toPay = new ToPay(0.0);
     }
 
+    private function toSecond(string $h, string $m): int
+    {
+        return (intval($h)*60) + intval($m);
+    }
+
     /**
      * The algorithm of this method evaluates the amount to be paid per hour worked by inverting
      * the hours of the working day and taking the coincidence of the entry time and at the same
      * time calculating the time difference against the end of the range of the working day
      *
-     * @param DateTime $workTimeInit
-     * @param DateTime $workTimeEnd
-     * @param $hourly
+     * @param float $toPay
+     * @param array $hourly
+     * @param int $hoursWorked
+     * @param $initTime
      * @return float
      */
-    private function sumValue(DateTime $workTimeInit, DateTime $workTimeEnd, $hourly):float
+    private function sumRecursive(float $toPay, array $hourly, int $hoursWorked, $initTime):float
     {
-        $interval = $workTimeInit->diff($workTimeEnd);
-        $initTime = $workTimeInit->format("H:i");
-        //$endTime = $workTimeEnd->format("H:i");
-
-        $hoursWorked = $interval->format("%h:%i");
-        $toPay = 0;
-        foreach (array_reverse($hourly) as $betweenHour => $salary) {
+        foreach ($hourly as $betweenHour => $salary) {
             $betweenData = explode('-', $betweenHour);
-            $betweenData[1] = ($betweenData[1] == !"00:00")?$betweenData[1]:"24:00"; //replacement 0 hours for 24
-            if ($initTime>=$betweenData[0]) {//check if the start time of the working day is greater than or equal to that of a work period
-                $diffHour = $betweenData[1]-$initTime;// obtain the time difference and it is calculated against the difference of the working period to know if values are generated from another period
-                if ($diffHour - $hoursWorked > 0) {
-                    $houMin = explode(':',$hoursWorked);
-                    $toPay += ($salary / 60) * ((float)str_replace(':','', ($houMin[0] * 60 + $houMin[1])));
-                    break;
-                }
+            if ($initTime>=$betweenData[0]) {
+                $diffHour = $toPay>0?($betweenData[1]-$initTime)+1:($betweenData[1]-$initTime);
+                unset($hourly[$betweenHour]);
+                return (($diffHour - $hoursWorked) > 0)
+                    ? $toPay += $this->calculateSalary($salary, $hoursWorked)
+                    : $this->sumRecursive(
+                        $toPay += $this->calculateSalary($salary, $diffHour)
+                        , $hourly, ($hoursWorked-$diffHour), ($initTime+$diffHour)+1);
             }
         }
         return $toPay;
     }
 
+    private function calculateSalary(float $salaryHour, int $seconds):float
+    {
+        return floatval(($salaryHour / 60) * $seconds);
+    }
+
     public function calculate(): void
     {
         $toPay = 0;
-        foreach ($this->payload as $keyDay => $day) {
-
-            $toPay += $this->sumValue(
-                $day[0],
-                $day[1],
-                $this->getHourly()[$this->isWeekend($day[0])?'weekend':'weekday']
+        foreach ($this->payload as $keyDay => $workTime) {
+            $initTime = $this->toSecond(
+                $workTime[0]->format("H"),
+                $workTime[0]->format("i")
             );
+            $endTime = $this->toSecond(
+                $workTime[1]->format("H"),
+                $workTime[1]->format("i")
+            );
+            $hoursWorked = $endTime - $initTime;
+            $hourlyReverse = array_reverse($this->getHourly()[$this->isWeekend($workTime[0])?'weekend':'weekday']);
+            $toPay += $this->sumRecursive(0, $hourlyReverse, $hoursWorked, $initTime);
         }
 
         $this->toPay->setValue($toPay);
